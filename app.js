4d83208ad427ad5b1d224cd2216b5e8d50837d8d
WangFanhua <18915636179@163.com>
2017-05-08 23:25:40 +0800

    增加post请求

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
 
 const router = new Router();
 const app = new Koa();
@@ -30,11 +31,14 @@ const app = new Koa();
 //     }
 // });
 
+app.use(bodyParser());
+
 // logger request url
 app.use(async (ctx, next) => {
     console.log(`Process ${ctx.method}---${ctx.url}`);
     await next();
 });
+
 
 router.get('/hello/:name', async (ctx, next) => {
     let name = ctx.params.name;
@@ -42,7 +46,26 @@ router.get('/hello/:name', async (ctx, next) => {
 });
 
 router.get('/', async (ctx, next) => {
-    ctx.body = '<h1>Index</h1>';
+    // ctx.body = '<h1>Index</h1>';
+    ctx.body = `
+        <h1>Index</h1>
+        <form action="/signin" method="post">
+            <p>Name: <input name="name" value="koa"/></p>
+            <p>Password: <input name="password" type="password"/></p>
+            <p><input type="submit" value="Submit"/></p>
+        </form>
+    `;
+});
+
+router.post('/signin', async (ctx, next) => {
+    let name = ctx.request.body.name;
+    let password = ctx.request.body.password;
+    console.log(`signin with name: ${name},password: ${password}`);
+    if (name === 'koa' && password === '123456') {
+        ctx.body = `<h1>Welcome, ${name}</h1>`;
+    } else {
+        ctx.body = '<h1>Login Fail</h1><a href="/">Try again</a>';
+    }
 });
 
 app.use(router.routes());
