fa4b18867bd270baebc256a42e463c516e2b1c28
WangFanhua <18915636179@163.com>
2017-05-08 23:08:54 +0800

    引入koa-router处理路由

--- app.js
+++ app.js
@@ -2,30 +2,31 @@ const Koa = require('koa');
 
 const app = new Koa();
 
-app.use(async (ctx, next) => {
-    console.log(`${ctx.method}--${ctx.url}`);
-    await next();
-});
+// 自己判断路径
+// app.use(async (ctx, next) => {
+//     if (ctx.path === '/') {
+//         // 不再向下传递
+//         ctx.body = 'index page';
+//     } else {
+//         await next();
+//     }
+// });
 
-app.use(async (ctx, next) => {
-    const start = (new Date()).getTime();
-    await next();
+// app.use(async (ctx, next) => {
+//     if (ctx.path === '/test') {
+//         ctx.body = 'test page';
+//     } else {
+//         await next();
+//     }
+// });
 
-    const ms = (new Date()).getTime() - start;
-    console.log(`Time: ${ms}`);
-});
-
-app.use(async (ctx, next) => {
-    await next();
-    console.log('我之后才打印');
-    ctx.type = 'text/html';
-    ctx.body = '<h1>Hello Koa2</h1>';
-});
-
-app.use(async (ctx, next) => {
-    console.log('我先打印');
-    await next();
-});
+// app.use(async (ctx, next) => {
+//     if (ctx.path === '/error') {
+//         ctx.body = 'error page';
+//     } else {
+//         await next();
+//     }
+// });
 
 app.listen(3000);
 console.log('app started on port 3000');
